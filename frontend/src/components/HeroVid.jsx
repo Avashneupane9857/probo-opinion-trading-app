04b9103c649a513f3522a7f28b1244f254feee7b
Avashneupane9857 <juisch20220011252@gmail.com>
2024-10-26 01:49:38 +0530

    :lipstick: HeroVid

--- frontend/src/components/HeroVid.jsx
+++ frontend/src/components/HeroVid.jsx
@@ -2,18 +2,26 @@ import { vid } from "../assets";
 
 function HeroVid() {
   return (
-    <div className="bg-[#262626]">
-      <div className="w-[80%] mx-auto flex">
-        <div className="w-[50%]">
-          <h1>Samachar Vichaar Vyapaar</h1>
-          <h1>Trade and Grow</h1>
-          <h1>Invest in your opinions about future events and use your knowledge to trade & benefit.</h1>
+    <div className="bg-[#262626] h-[650px]">
+      <div className="w-[95%] mx-auto flex ">
+        <div className="w-[50%]  ">
+          <div className="flex gap-6 mt-44 mb-4">
+            <h1 className="text-[55px] text-slate-400">Samachar</h1>
+            <p className="text-[55px] text-white"> Vichaar </p>
+            <p className="text-[55px] text-slate-400">Vyapaar</p>
+          </div>
+          <h1 className=" mb-4 text-[34px] text-white">Trade and Grow</h1>
+          <h1 className="text-[32px] text-white">
+            Invest in your opinions about future events and use your knowledge
+            to trade & benefit.
+          </h1>
         </div>{" "}
-        <div className="border-4 rounded-2xl border-black">
+        <div className=" flex items-center pl-44 mt-8">
           <video
-            className="w-[90px] rounded-2xl h-[200px]"
+            className="p-5 w-[300px] h-[600px]  rounded-[80px] "
             autoPlay
             loop
+            muted
             src={vid}
           ></video>
         </div>
@@ -23,3 +31,5 @@ function HeroVid() {
 }
 
 export default HeroVid;
+
+//whitespace-nowrap
